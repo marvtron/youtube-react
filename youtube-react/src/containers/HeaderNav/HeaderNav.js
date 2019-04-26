8a4db866d7054757fae1fa79a610405729548e61
Marv_Tron <marvinatryonjr@gmail.com>
2019-04-26 12:35:33 -0700

    __tests__

--- youtube-react/src/containers/HeaderNav/HeaderNav.js
+++ youtube-react/src/containers/HeaderNav/HeaderNav.js
@@ -4,25 +4,39 @@ import './HeaderNav.scss';
 import logo from '../../assets/images/logo.jpg';
 
 export class HeaderNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+    };
+  }
   render() {
     return (
+      // 1
       <Menu borderless className='top-menu' fixed='top'>
+        {/* 2 */}
         <Menu.Item header className='logo'>
-            <Image src={logo} size='tiny'/>
+          <Image src={logo} size='tiny'/>
         </Menu.Item>
+        {/* 3 */}
         <Menu.Menu className='nav-container'>
           <Menu.Item className='search-input'>
-            <Form>
+            <Form onSubmit={this.onSubmit}>
+              {/* 4 */}
               <Form.Field>
                 <Input placeholder='Search'
                        size='small'
                        action='Go'
+                       value={this.state.query}
+                       onChange={this.onInputChange}
                 />
               </Form.Field>
             </Form>
           </Menu.Item>
+          {/* 5 */}
           <Menu.Menu position='right'>
             <Menu.Item>
+              {/* 6 */}
               <Icon className='header-icon' name='video camera' size='large'/>
             </Menu.Item>
             <Menu.Item>
@@ -34,6 +48,7 @@ export class HeaderNav extends React.Component {
             <Menu.Item>
               <Icon className='header-icon' name='alarm' size='large'/>
             </Menu.Item>
+            {/* 7*/}
             <Menu.Item name='avatar'>
               <Image src='http://via.placeholder.com/80x80' avatar/>
             </Menu.Item>
@@ -42,6 +57,16 @@ export class HeaderNav extends React.Component {
       </Menu>
     );
   }
+  onInputChange = (event) => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
+
+  onSubmit = () => {
+    const escapedSearchQuery = encodeURI(this.state.query);
+    this.props.history.push(`/results?search_query=${escapedSearchQuery}`);
+  };
 }
 
 export default HeaderNav;
