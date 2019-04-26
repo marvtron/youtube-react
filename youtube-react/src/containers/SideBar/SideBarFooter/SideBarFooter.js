76fa95366c9616ab21ac18cb8f984136c22cce45
Marv_Tron <marvinatryonjr@gmail.com>
2019-04-26 11:28:53 -0700

    finished up the sidebar

---
+++ youtube-react/src/containers/SideBar/SideBarFooter/SideBarFooter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './SideBarFooter.scss'
+
+export function SideBarFooter() {
+  return (
+    <React.Fragment>
+      <div className='footer-block'>
+        <div>About Press Copyright</div>
+        <div>Creators Advertise</div>
+        <div>Developers +MyTube</div>
+        <div>Legal</div>
+      </div>
+      <div className='footer-block'>
+        <div>Terms Privacy</div>
+        <div>Policy & Safety</div>
+        <div>Test New Features</div>
+      </div>
+      <div className='footer-block'>
+        <div>All prices include VAT</div>
+      </div>
+      <div className='footer-block'>
+        <div>© marvtrondev - A Youtube clone for educational purposes under fair use.</div>
+      </div>
+    </React.Fragment>
+  );
+}
